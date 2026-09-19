df0844256e208c86963b58e66479210ae498b5b6
juhdtech-cmd <juhd.tech@gmail.com>
2026-09-19 14:23:53 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const btn=document.getElementById("lang"), menu=document.getElementById("langMenu");
+const btn=document.getElementById("lang"),menu=document.getElementById("langMenu");
 btn.onclick=()=>menu.classList.toggle("open");
-function setLang(lang){document.querySelectorAll("[data-ms]").forEach(el=>{el.innerHTML=el.dataset[lang]});btn.innerHTML=(lang==="ms"?"🇲🇾 <b>BM</b>⌄":"🇬🇧 <b>EN</b>⌄");document.documentElement.lang=lang;localStorage.setItem("uk-lang",lang);menu.classList.remove("open")}
+function setLang(lang){document.querySelectorAll("[data-ms]").forEach(el=>el.innerHTML=el.dataset[lang]);btn.innerHTML=(lang==="ms"?"🇲🇾 <b>BM</b>⌄":"🇬🇧 <b>EN</b>⌄");document.documentElement.lang=lang;localStorage.setItem("uk-lang",lang);menu.classList.remove("open")}
 document.querySelectorAll("[data-lang]").forEach(x=>x.onclick=()=>setLang(x.dataset.lang));
 setLang(localStorage.getItem("uk-lang")||"ms");
